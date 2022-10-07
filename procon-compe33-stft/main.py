822304db96571b1d814ba8a0877825aba7cc47a8
myst0123 <52592597+myst-0123@users.noreply.github.com>
2022-10-07 13:37:29 +0900

    Update ファイルに出力できるようにした

--- procon-compe33-stft/main.py
+++ procon-compe33-stft/main.py
@@ -2,32 +2,40 @@ import numpy as np
 from scipy import signal
 from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
-import wave
+import soundfile as sf
 
 def main():
-    data, fs = wav_read('../sound-data/J01.wav')
-    print(max(data), min(data))
+    for i in range(1, 10):
+        print(i)
+        data, fs = wav_read('../sound-data/J0' + str(i) + '.wav')
+        Sxx = stft(data, fs)
+        np.savetxt('../data/J' + str(i) + '.txt', Sxx, delimiter=' ')
+    for i in range(10, 45):
+        print(i)
+        data, fs = wav_read('../sound-data/J' + str(i) + '.wav')
+        Sxx = stft(data, fs)
+        np.savetxt('../data/J' + str(i) + '.txt', Sxx, delimiter=' ')
 
-    
-    
-    f, t, Sxx = signal.spectrogram(data, fs, window='hamming', nperseg=512)
-
-    Sxx2 = np.log10(Sxx)
-    print(Sxx2.max(), Sxx2.min())
-    
-    plt.pcolormesh(t, f, Sxx2)
-    plt.ylabel('Frequency [Hz]')
-    plt.xlabel('Time [sec]')
-    cbar = plt.colorbar()
-    cbar.ax.set_ylabel('intensity [dB]')
-    plt.show()
+    for i in range(1, 10):
+        print(i)
+        data, fs = wav_read('../sound-data/E0' + str(i) + '.wav')
+        Sxx = stft(data, fs)
+        np.savetxt('../data/E' + str(i) + '.txt', Sxx, delimiter=' ')
+    for i in range(10, 45):
+        print(i)
+        data, fs = wav_read('../sound-data/E' + str(i) + '.wav')
+        Sxx = stft(data, fs)
+        np.savetxt('../data/E' + str(i) + '.txt', Sxx, delimiter=' ')
 
 def wav_read(path):
-    wav = wave.open(path)
-    buffer = wav.readframes(-1)
-    data = np.frombuffer(buffer, dtype='int16')
-    fs = wav.getframerate()
+    data, fs = sf.read(path)
     return data, fs
+
+def stft(data, fs):
+    f, t, Sxx = signal.spectrogram(data, fs, window='han', nperseg=512)
+    Sxx = 20*np.log10(Sxx)
+    Sxx = Sxx - Sxx.max()
+    return Sxx
 
 if __name__ == '__main__':
     main()
