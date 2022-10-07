c35c743f1e32871c6a5d167526cf54a2a467319a
myst0123 <52592597+myst-0123@users.noreply.github.com>
2022-10-07 14:40:31 +0900

    Add 競技本番用の関数の作成

---
+++ procon-compe33-stft/stft.py
@@ -0,0 +1,23 @@
+import numpy as np
+from scipy import signal
+from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
+import soundfile as sf
+
+def main():
+    data, fs = wav_read('../problem.wav')
+    Sxx = stft(data, fs)
+    np.savetxt('../problem.txt', Sxx, delimiter=' ', fmt='%d')
+
+def wav_read(path):
+    data, fs = sf.read(path)
+    return data, fs
+
+def stft(data, fs):
+    f, t, Sxx = signal.spectrogram(data, fs, window='han', nperseg=512)
+    Sxx = 20*np.log10(Sxx)
+    Sxx = Sxx - Sxx.max()
+    return Sxx
+
+if __name__ == '__main__':
+    main()
