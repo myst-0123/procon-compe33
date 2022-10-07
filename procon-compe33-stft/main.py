c8588bda4d38f1e45e57af4c6d4aec1030c0eeda
myst0123 <52592597+myst-0123@users.noreply.github.com>
2022-10-07 15:02:01 +0900

    Fix 真ん中の2秒間だけデータを取るように

--- procon-compe33-stft/main.py
+++ procon-compe33-stft/main.py
@@ -8,23 +8,23 @@ def main():
     for i in range(1, 10):
         print(i)
         data, fs = wav_read('../sound-data/J0' + str(i) + '.wav')
-        Sxx = stft(data, fs)
+        Sxx = stft(data[48000*0.5:48000*2.5], fs)
         np.savetxt('../data/J' + str(i) + '.txt', Sxx, delimiter=' ', fmt='%d')
     for i in range(10, 45):
         print(i)
         data, fs = wav_read('../sound-data/J' + str(i) + '.wav')
-        Sxx = stft(data, fs)
+        Sxx = stft(data[48000*0.5:48000*2.5], fs)
         np.savetxt('../data/J' + str(i) + '.txt', Sxx, delimiter=' ', fmt='%d')
 
     for i in range(1, 10):
         print(i)
         data, fs = wav_read('../sound-data/E0' + str(i) + '.wav')
-        Sxx = stft(data, fs)
+        Sxx = stft(data[48000*0.5:48000*2.5], fs)
         np.savetxt('../data/E' + str(i) + '.txt', Sxx, delimiter=' ', fmt='%d')
     for i in range(10, 45):
         print(i)
         data, fs = wav_read('../sound-data/E' + str(i) + '.wav')
-        Sxx = stft(data, fs)
+        Sxx = stft(data[48000*0.5:48000*2.5], fs)
         np.savetxt('../data/E' + str(i) + '.txt', Sxx, delimiter=' ', fmt='%d')
 
 def wav_read(path):
@@ -33,9 +33,9 @@ def wav_read(path):
 
 def stft(data, fs):
     f, t, Sxx = signal.spectrogram(data, fs, window='han', nperseg=512)
-    print(Sxx)
     Sxx = 20*np.log10(Sxx)
     Sxx = Sxx - Sxx.max()
+    print(Sxx.size)
     return Sxx
 
 if __name__ == '__main__':
