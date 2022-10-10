87ae8204a8e1d927c7d2d650e032a728c4a3f08a
myst0123 <52592597+myst-0123@users.noreply.github.com>
2022-10-10 11:35:34 +0900

    Update いろいろ調整

--- procon-conpe33-compare/main.cpp
+++ procon-conpe33-compare/main.cpp
@@ -6,13 +6,25 @@
 
 using namespace std;
 
+bool comp(pair<int, double> lhs, pair<int, double> rhs)
+{
+    return lhs.second < rhs.second;
+}
+
 int main(int argc, char *argv[])
 {
     bool is_japanese = stoi(argv[1]);
 
     vector<vector<int>> problem_data;
+    vector<pair<int, double>> result; 
 
     problem_data = read_file("../problem.txt");
 
-    compare(problem_data, is_japanese);
+    result = compare(problem_data, is_japanese);
+    sort(result.rbegin(), result.rend(), comp);
+    cout << "---------result------------" << endl;
+    for (auto i: result)
+    {
+        cout << i.first << " " << i.second << endl;
+    }
 }
