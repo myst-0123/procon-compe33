18f65dae7d24592d59c954093246c5247bff1395
myst0123 <52592597+myst-0123@users.noreply.github.com>
2022-10-15 23:56:36 +0900

    Update 比較部分の改良

--- procon-conpe33-compare/compare2.cpp
+++ procon-conpe33-compare/compare2.cpp
@@ -2,7 +2,7 @@
 
 vector<pair<string, double>> compare2(vector<vector<int>> problem_data)
 {
-    cout << "----Function compare----" << endl;
+    cout << "----Function compare2----" << endl;
     vector<pair<string, double>> match_rate;
 
     int problem_length = problem_data[1].size();
@@ -11,73 +11,78 @@ vector<pair<string, double>> compare2(vector<vector<int>> problem_data)
 
     for (int i = 1; i <= 44; i++)
     {
-        vector<vector<int>> compare_data;
         vector<double> avg_match_rate;
-
-        compare_data = read_file("../data/Jb" + to_string(i) + ".txt");
-
-        int compare_length = compare_data[1].size();
-
-        int correct, all;
-
-        for (int j = 0; j < problem_length - compare_length; j++)
+        for (int start = 0; start <= 144000; start += 4800)
         {
-            correct = 0;
-            all = 0;
-            for (int k = 1; k < freq-1; k++)
+            cout << "J" << i << "-" << start << endl;
+            vector<vector<int>> compare_data;
+            compare_data = read_file("../data/Jb" + to_string(i) + "-" + to_string(start) + ".txt");
+
+            int compare_length = compare_data[1].size();
+
+            int correct, all;
+
+            for (int j = 0; j < problem_length - compare_length; j++)
             {
-                for (int m = 0; m < compare_length; m++)
+                correct = 0;
+                all = 0;
+                for (int k = 1; k < freq-1; k++)
                 {
-                    if (compare_data[k][m] == 1)
+                    for (int m = 0; m < compare_length; m++)
                     {
-                        all++;
-                        if(problem_data[k][m + j] == 1)
+                        if (compare_data[k][m] == 1)
                         {
-                            correct++;
+                            all++;
+                            if(problem_data[k][m + j] == 1)
+                            {
+                                correct++;
+                            }
                         }
                     }
                 }
+                avg_match_rate.push_back(correct / (double)all);
             }
-            avg_match_rate.push_back(correct / (double)all);
         }
+
         sort(avg_match_rate.rbegin(), avg_match_rate.rend());
         match_rate.push_back(make_pair("J" + to_string(i), avg_match_rate[0]));
-        cout << avg_match_rate[0] << endl;
     }
     for (int i = 1; i <= 44; i++)
     {
-        vector<vector<int>> compare_data;
         vector<double> avg_match_rate;
-
-        compare_data = read_file("../data/Eb" + to_string(i) + ".txt");
-
-        int compare_length = compare_data[1].size();
-
-        int correct, all;
-
-        for (int j = 0; j < problem_length - compare_length; j++)
+        for (int start = 0; start <= 144000; start += 4800)
         {
-            correct = 0;
-            all = 0;
-            for (int k = 1; k < freq-1; k++)
+            cout << "E" << i << "-" << start << endl;
+            vector<vector<int>> compare_data;
+            compare_data = read_file("../data/Eb" + to_string(i) + "-" + to_string(start) + ".txt");
+
+            int compare_length = compare_data[1].size();
+
+            int correct, all;
+
+            for (int j = 0; j < problem_length - compare_length; j++)
             {
-                for (int m = 0; m < compare_length; m++)
+                correct = 0;
+                all = 0;
+                for (int k = 1; k < freq-1; k++)
                 {
-                    if (compare_data[k][m] == 1)
+                    for (int m = 0; m < compare_length; m++)
                     {
-                        all++;
-                        if(problem_data[k][m + j] == 1)
+                        if (compare_data[k][m] == 1)
                         {
-                            correct++;
+                            all++;
+                            if(problem_data[k][m + j] == 1)
+                            {
+                                correct++;
+                            }
                         }
                     }
                 }
+                avg_match_rate.push_back(correct / (double)all);
             }
-            avg_match_rate.push_back(correct / (double)all);
         }
         sort(avg_match_rate.rbegin(), avg_match_rate.rend());
         match_rate.push_back(make_pair("E" + to_string(i), avg_match_rate[0]));
-        cout << avg_match_rate[0] << endl;
     }
 
     return match_rate;
